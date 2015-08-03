2b22aa993708124620bfd1154a2fddb3ef6fdcd4
Nikita <mysteryum@mail.ru>
2015-08-03 12:52:44 +0300

    new file:   account.php 	new file:   account_edit.php 	new file:   account_history.php 	new file:   account_history_info.php 	new file:   account_password.php 	new file:   address_book.php 	new file:   address_book_process.php 	new file:   advanced_search.php 	new file:   advanced_search_result.php 	new file:   affiliate_affiliate.php 	new file:   affiliate_banners.php 	new file:   affiliate_clicks.php 	new file:   affiliate_contact.php 	new file:   affiliate_details.php 	new file:   affiliate_details_ok.php 	new file:   affiliate_help1.php 	new file:   affiliate_help2.php 	new file:   affiliate_help3.php 	new file:   affiliate_help4.php 	new file:   affiliate_help5.php 	new file:   affiliate_help6.php 	new file:   affiliate_help7.php 	new file:   affiliate_help8.php 	new file:   affiliate_logout.php 	new file:   affiliate_password_forgotten.php 	new file:   affiliate_payment.php 	new file:   affiliate_sales.php 	new file:   affiliate_show_banner.php 	new file:   affiliate_signup.php 	new file:   affiliate_signup_ok.php 	new file:   affiliate_summary.php 	new file:   article_info.php 	new file:   articles.php 	new file:   articles_new.php 	new file:   ask_a_question.php 	new file:   aviso.php 	new file:   captcha.php 	new file:   checkout.php 	new file:   checkout_confirmation.php 	new file:   checkout_payment.php 	new file:   checkout_payment_address.php 	new file:   checkout_process.php 	new file:   checkout_shipping.php 	new file:   checkout_shipping_address.php 	new file:   checkout_success.php 	new file:   comparison.php 	new file:   cookie_usage.php 	new file:   create_account.php 	new file:   db_error.htm 	new file:   deltakey.php 	new file:   down_for_maintenance.php 	new file:   download.php 	new file:   easypay.php 	new file:   faq.php 	new file:   favicon.ico 	new file:   featured.php 	new file:   google_sitemap.php 	new file:   gv_redeem.php 	new file:   gv_send.php 	new file:   ik.php 	new file:   index.php 	new file:   index_ajax.php 	new file:   intellectmoney.php 	new file:   ipn.php 	new file:   kaznachey.processing.php 	new file:   kaznachey.success.php 	new file:   kvitancia.php 	new file:   liqpay.php 	new file:   login.php 	new file:   logoff.php 	new file:   manager.php 	new file:   market.php 	new file:   media_content.php 	new file:   moneyua.php 	new file:   news.php 	new file:   newsletter.php 	new file:   onpay.php 	new file:   packingslip.php 	new file:   password_double_opt.php 	new file:   pay2pay.php 	new file:   paymaster.php 	new file:   payonlinesystem.php 	new file:   platron_invoice.php 	new file:   popup_content.php 	new file:   popup_coupon_help.php 	new file:   popup_cvv.php 	new file:   popup_search_help.php 	new file:   print_order.php 	new file:   print_product_info.php 	new file:   prochange.php 	new file:   product_info.php 	new file:   product_reviews.php 	new file:   product_reviews_info.php 	new file:   product_reviews_write.php 	new file:   products_barcode.php 	new file:   products_filter.php 	new file:   products_new.php 	new file:   protect.inc.php 	new file:   qiwi_rest.php 	new file:   rbkmoney.php 	new file:   redirect.php 	new file:   redirector.php 	new file:   reviews.php 	new file:   robots.txt 	new file:   robox.php 	new file:   rss.css 	new file:   rss.xsl 	new file:   rss2.php 	new file:   rss_xsl.css 	new file:   sc_checkout_confirmation.php 	new file:   schet.php 	new file:   send_order.php 	new file:   shop_content.php 	new file:   shopping_cart.php 	new file:   shops.dtd 	new file:   specials.php 	new file:   ssl_check.php 	new file:   validateSwedPayment.php 	new file:   vam_rss2_info.php 	new file:   vamshop.txt 	new file:   version.txt 	new file:   wallet_one.php 	new file:   webmoney.php 	new file:   webpay.php 	new file:   yandex.php 	new file:   z_payment.php
    	LICENSE
    	admin/
    	download/
    	export/
    	ext/
    	images/
    	import/
    	inc/
    	includes/
    	install/
    	jscript/
    	lang/
    	media/
    	pub/
    	templates/

---
+++ ik.php
@@ -0,0 +1,94 @@
+<?php
+/*------------------------------------------------------------------------------
+  $Id: ik.php 1310 2010-05-06 19:20:03 VaM $
+
+   VaM Shop - open source ecommerce solution
+   http://vamshop.ru
+   http://vamshop.com
+
+   Copyright (c) 2010 VaM Shop
+  -----------------------------------------------------------------------------
+   based on:
+   (c) 2005 Vetal (robox.php,v 1.48 2003/05/27); metashop.ru
+
+  Released under the GNU General Public License
+------------------------------------------------------------------------------*/
+
+function get_var($name, $default = 'none') {
+  return (isset($_GET[$name])) ? $_GET[$name] : ((isset($_POST[$name])) ? $_POST[$name] : $default);
+}
+
+require('includes/application_top.php');
+require (DIR_WS_CLASSES.'order.php');
+
+function ikGetSign($post)
+{
+	$aParams = array();
+	foreach ($post as $key => $value)
+	{
+		if (!preg_match('/ik_/', $key))
+			continue;
+		$aParams[$key] = $value;
+	}
+
+	unset($aParams['ik_sign']);
+
+		$key = MODULE_PAYMENT_IK_SECRET_KEY;
+
+	ksort ($aParams, SORT_STRING);
+	array_push($aParams, $key);
+	$signString = implode(':', $aParams);
+	$sign = base64_encode(md5($signString, true));
+	return $sign;
+}
+
+// logging
+
+//$fp = fopen('ik.log', 'a+');
+//$str=date('Y-m-d H:i:s').' - ';
+//foreach ($_POST as $vn=>$vv) {
+//  $str.=$vn.'='.$vv.';';
+//}
+
+//fwrite($fp, $str."\n");
+//fclose($fp);
+
+// Проверка id магазина
+if(MODULE_PAYMENT_IK_SHOP_ID !== $ik_co_id)
+	die('error: ik_co_id');
+
+$sign = ikGetSign($_POST);
+
+if ($ik_sign === $sign && $ik_inv_st == 'success')
+{
+	// данные заказа
+	$order = new order((int)$ik_pm_no);
+	$ikCurrency = (MODULE_PAYMENT_IK_CURRENCY == 'RUB') ? 'RUR' : MODULE_PAYMENT_IK_CURRENCY;
+
+	global $vamPrice;
+	$orderTotal = number_format($vamPrice->CalculateCurrEx($order->info['total'], $ikCurrency), 2, '.', '');
+
+	// Проверяем стоимость
+	if($ik_am != $orderTotal OR $ik_am <= 0)
+		die("error: ik_am");
+
+	$sql_data_array = array
+	(
+		'orders_status' => MODULE_PAYMENT_IK_ORDER_STATUS_ID
+	);
+	vam_db_perform('orders', $sql_data_array, 'update', "orders_id='".(int)$ik_pm_no."'");
+
+	$sql_data_arrax = array
+	(
+		'orders_id' => (int)$ik_pm_no,
+		'orders_status_id' => MODULE_PAYMENT_IK_ORDER_STATUS_ID,
+		'date_added' => 'now()',
+		'customer_notified' => '0',
+		'comments' => 'InterKassa accepted this order payment '.vam_db_prepare_input($ik_pw_via)
+	);
+	vam_db_perform('orders_status_history', $sql_data_arrax);
+
+	echo 'OK'.$ik_pm_no;
+}
+else
+	die('error: ik_sign or ik_inv_st');
